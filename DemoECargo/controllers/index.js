ca42919b9bdadc78f74bc0e774faa9b421503858
CangPham <cang.phamsy@harveynash.vn>
2016-04-05 21:27:37 +0700

    init

---
+++ DemoECargo/controllers/index.js
@@ -0,0 +1,8 @@
+﻿//index.js
+(function (controllers) {
+	"use strict";
+	var productsController = require('./productsController.js');
+	controllers.init = function (app) { 
+		productsController.init(app);
+	};
+})(module.exports);
---
+++ DemoECargo/controllers/productsController.js
@@ -0,0 +1,16 @@
+﻿//productsController
+(function (productsController) {
+	"use strict";
+	
+	//var data = require('../data');
+
+	productsController.init = function (app) {
+		app.get("/api/products/", function (req, res) {
+			//data.getProducts(function (err, results) {
+			//	res.json(results);
+			//});
+            res.json({ "test": "hello" });
+		});
+	};
+
+})(module.exports);
